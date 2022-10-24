3265c0a426e53a2ad4f1b6c39df9fb14fdd49182
RayOneThousand <rohan_samuel@sfu.ca>
2022-10-23 22:57:36 -0700

    Trying Dash and clean up

--- Invasive_1/Assets/Scripts/PlayerLocomotion.cs
+++ Invasive_1/Assets/Scripts/PlayerLocomotion.cs
@@ -37,25 +37,12 @@ namespace RNS
         public void Update()
         {
             float delta = Time.deltaTime;
+
             inputHandler.TickInput(delta);
-
-            moveDirection = cameraObject.forward * inputHandler.vertical;
-            moveDirection += cameraObject.right * inputHandler.horizontal;
-            moveDirection.Normalize();
-            moveDirection.y = 0;
-
-            float speed = movementSpeed;
-            moveDirection *= speed;
-
-            Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
-            rigidbody.velocity = projectedVelocity;
-
-           animatorHandler.UpdateAnimatorValues(inputHandler.moveAmount, 0);
-
-            if (animatorHandler.canRotate)
-                {
-                    HandleRotation(delta);
-                }
+            HandleMovement(delta);
+            HandleDashingAndCrouching(delta);
+           
+            
                 
         }
 
@@ -83,6 +70,51 @@ namespace RNS
             Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
 
             myTransform.rotation = targetRotation;
+        }
+
+        public void HandleMovement(float delta)
+        {
+            moveDirection = cameraObject.forward * inputHandler.vertical;
+            moveDirection += cameraObject.right * inputHandler.horizontal;
+            moveDirection.Normalize();
+            moveDirection.y = 0;
+
+            float speed = movementSpeed;
+            moveDirection *= speed;
+
+            Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+            rigidbody.velocity = projectedVelocity;
+
+            animatorHandler.UpdateAnimatorValues(inputHandler.moveAmount, 0);
+
+            if (animatorHandler.canRotate)
+            {
+                HandleRotation(delta);
+            }
+        }
+
+        public void HandleDashingAndCrouching(float delta)
+        {
+            if (animatorHandler.anim.GetBool("isInteracting"))
+                return;
+
+            if (inputHandler.dashFlag)
+            {
+                moveDirection = cameraObject.forward * inputHandler.vertical;
+                moveDirection += cameraObject.right * inputHandler.horizontal;
+
+                if (inputHandler.moveAmount > 0)
+                {
+                    animatorHandler.PlayTargetAnimation("Dashing", true);
+                    moveDirection.y = 0;
+                    Quaternion dashRotation = Quaternion.LookRotation(moveDirection);
+                    myTransform.rotation = dashRotation;
+                }
+                else
+                {
+                    Debug.Log("That's weird");
+                }
+            }
         }
            #endregion
 
