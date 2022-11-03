d33f093da1b9e5997307a2f3f6f53e1df1b52453
RayOneThousand <rohan_samuel@sfu.ca>
2022-11-02 20:19:10 -0700

    SeedShooterAI

---
+++ Invasive_1/Assets/DeathHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHandler : MonoBehaviour
+{
+    public GameObject vine1, vine2, vine3;
+    private Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+        animator = GetComponentInChildren<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vine1 == null && vine2 == null && vine3 == null)
+        {
+            animator.SetTrigger("OnDeath");
+        }
+    }
+}
--- Invasive_1/Assets/Scripts/Vines/VineAI.cs
+++ Invasive_1/Assets/Scripts/Vines/VineAI.cs
@@ -11,33 +11,86 @@ namespace RNS
         public float rotationSpeed = 2f;
 
         private StateMachine fsm;
-        public float hearingRange = 5f;
+        public float hearingRange = 6f;
+        public float attackRange = 3f;
+
+        private Animator animator;
 
         public Transform target;
 
 
-        float PlayerDirection()
+        float DistanceToPlayer()
         {
             Vector3 player = target.position;
-            return Vector2.Angle(transform.position, player);
+            return Vector2.Distance(transform.position, player);
         }
 
         void RotateTowardsPlayer()
         {
             Vector3 player = target.position;
             transform.LookAt(target);
+            animator.SetTrigger("BackToIdle");
+        }
+
+        void AttackPlayer()
+        {
+            Vector3 player = target.position;
+            transform.LookAt(target);
+            animator.SetTrigger("OnAttackRange");
         }
 
 
         void Start()
         {
-            fsm = new StateMachine();
+            animator = GetComponent<Animator>();
+
+            fsm = new StateMachine(this);
+
+            fsm.AddState("Listen", new State());
+
+            fsm.AddState("LookAtPlayer", new State(
+                onLogic: (state) => RotateTowardsPlayer()));
+
+            fsm.AddState("AttackPlayer", new State(
+                onLogic: (state) => AttackPlayer())) ;
+
+            fsm.SetStartState("Listen");
+
+            
+
+            fsm.AddTransition(new Transition(
+                "Listen",
+                "LookAtPlayer",
+                (transition) => DistanceToPlayer() < hearingRange
+                ));
+
+            fsm.AddTransition(new Transition(
+               "LookAtPlayer",
+               "Listen",
+               (transition) => DistanceToPlayer() > hearingRange
+               ));
+
+            fsm.AddTransition(new Transition(
+               "LookAtPlayer",
+               "AttackPlayer",
+               (transition) => DistanceToPlayer() < attackRange
+               ));
+
+            fsm.AddTransition(new Transition(
+               "AttackPlayer",
+               "LookAtPlayer",
+               (transition) => DistanceToPlayer() > attackRange
+               ));
+            
+            fsm.Init();
         }
 
         // Update is called once per frame
         void Update()
         {
-            RotateTowardsPlayer();
+            fsm.OnLogic();
+
+            
         }
     }
 }
