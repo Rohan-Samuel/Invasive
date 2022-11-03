3822c126ddf8bf6fd37c35d70ee4d5fd1d4ff6c0
JSafaei <45118336+JSafaei@users.noreply.github.com>
2022-11-02 23:41:05 -0700

    Adding shambler Zambie follow the player

--- Invasive_1/Assets/FollowScript.cs
+++ Invasive_1/Assets/FollowScript.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class FollowScript : MonoBehaviour
 {
+    UnityEngine.AI.NavMeshAgent agent;
+    public Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        agent.SetDestination(target.position);
         
     }
 }
