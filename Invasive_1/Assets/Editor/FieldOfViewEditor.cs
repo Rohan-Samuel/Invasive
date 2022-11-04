cbaaa8b43d3a762b45b181d41770341204811a2a
JSafaei <45118336+JSafaei@users.noreply.github.com>
2022-11-03 18:46:57 -0700

    Created Editor folder for editor code

--- Invasive_1/Assets/Editor/FieldOfViewEditor.cs
+++ Invasive_1/Assets/Editor/FieldOfViewEditor.cs
@@ -6,11 +6,9 @@ using UnityEditor;
 
 public class FieldOfViewEditor : Editor
 {
-
     private void OnSceneGUI()
     {
         FieldOfView fov = (FieldOfView) target;
-        Handles.color = Color.white;
         Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.radius);
 
         Vector3 viewAngle01 = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.angle / 2);
