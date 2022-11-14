9fcb4d431aab841dd6c68280e12bee3986af8918
Pyranthian <61473137+Pyranthian@users.noreply.github.com>
2022-11-13 19:03:45 -0800

    Added Note images and note script (unfinished)

---
+++ Invasive_1/Assets/Prefab/Notes/NoteScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class NoteScript : MonoBehaviour
+{
+
+    public Image noteImage;
+    public AudioClip pickupSound;
+    public AudioClip putAwaySound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        noteImage.enabled = false;
+
+    }
+
+    public void ShowNoteImage()
+    {
+        noteImage.enabled = true;
+        GetComponent<AudioSource>().PlayOneShot(pickupSound);
+
+    }
+
+    public void HideNoteImage()
+    {
+        noteImage.enabled = false;
+        GetComponent<AudioSource>().PlayOneShot(putAwaySound); 
+    }
+}
