0d1723440ba15d61f476e028db767f602ceb7a8e
Pyranthian <61473137+Pyranthian@users.noreply.github.com>
2022-11-14 01:13:35 -0800

    Added new cutscenes to start

---
+++ Invasive_1/Assets/Typewritereffect.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+/*
+public class Typewritereffect : MonoBehaviour 
+{
+    public float textspeed = 0.1f;
+    public string fullText;
+    private string currentText = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       StartCoroutine(ShowText(currentText)); 
+    }
+
+    IEnumerator ShowText()
+    {
+        for(int i = 0; i < fullText.Length+1; i++)
+        {
+            currentText = fullText.Substring(0,i);
+            this.getComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(textspeed);
+        }
+    }
+}
+*/
