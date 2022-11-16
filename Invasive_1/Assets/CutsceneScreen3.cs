88f079dce89dd871679b546719c398a4e0aed1e2
Pyranthian <61473137+Pyranthian@users.noreply.github.com>
2022-11-16 15:21:06 -0800

    Added new tutorial cutscenes

--- Invasive_1/Assets/CutsceneScreen3.cs
+++ Invasive_1/Assets/CutsceneScreen3.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CutsceneScreen2 : MonoBehaviour
+public class CutsceneScreen3 : MonoBehaviour
 {
 
     // Start is called before the first frame update
@@ -17,7 +17,7 @@ public class CutsceneScreen2 : MonoBehaviour
     
             if (Input.GetMouseButton(0))
             {
-                SceneManager.LoadScene(6);
+                SceneManager.LoadScene(7);
                 
             }
    
