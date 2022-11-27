f91d82607d9054bb215af752b7ba44db7af0a5d0
BretBauslaugh <106436296+BretBauslaugh@users.noreply.github.com>
2022-11-27 02:06:41 -0800

    Stealth system updated with walking

--- Invasive_1/Assets/Scripts/Player/IAmSneaky.cs
+++ Invasive_1/Assets/Scripts/Player/IAmSneaky.cs
@@ -8,6 +8,8 @@ public class IAmSneaky : MonoBehaviour
     public int AUDIO_STEALTH = 2;
 
     bool lightOn = true;
+
+    bool wDown = false, aDown = false, sDown = false, dDown = false, moving = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +32,7 @@ public class IAmSneaky : MonoBehaviour
             gameObject.GetComponent<StealthHandler>().setSound(AUDIO_STEALTH);
             gameObject.GetComponent<StealthHandler>().setSight(VISUAL_STEALTH);
         }
-        else if (Input.GetKeyDown(KeyCode.F)){
+        if (Input.GetKeyDown(KeyCode.F)){
             if(lightOn){
                 lightOn = false;
                 VISUAL_STEALTH--;
@@ -42,5 +44,46 @@ public class IAmSneaky : MonoBehaviour
                 gameObject.GetComponent<StealthHandler>().setSight(VISUAL_STEALTH);
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.W)){
+            wDown = true;
+        }
+        if (Input.GetKeyDown(KeyCode.A)){
+            aDown = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S)){
+            sDown = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D)){
+            dDown = true;
+        }
+        if (Input.GetKeyUp(KeyCode.W)){
+            wDown = false;
+        }
+        if (Input.GetKeyUp(KeyCode.A)){
+            aDown = false;
+        }
+        if (Input.GetKeyUp(KeyCode.S)){
+            sDown = false;
+        }
+        if (Input.GetKeyUp(KeyCode.D)){
+            dDown = false;
+        }
+
+        if(!sDown && !wDown && !aDown && !dDown && moving){
+            moving = false;
+            VISUAL_STEALTH--;
+            AUDIO_STEALTH--;
+            gameObject.GetComponent<StealthHandler>().setSound(AUDIO_STEALTH);
+            gameObject.GetComponent<StealthHandler>().setSight(VISUAL_STEALTH);
+        }
+        else if ((sDown || wDown || aDown || dDown) && !moving){
+            moving = true;
+            VISUAL_STEALTH++;
+            AUDIO_STEALTH++;
+            gameObject.GetComponent<StealthHandler>().setSound(AUDIO_STEALTH);
+            gameObject.GetComponent<StealthHandler>().setSight(VISUAL_STEALTH);
+        }
+
     }
 }
