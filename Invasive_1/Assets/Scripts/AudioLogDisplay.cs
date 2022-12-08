48ff6665750bfc4ca3751c0c81bf117a622a1b1d
BretBauslaugh <106436296+BretBauslaugh@users.noreply.github.com>
2022-12-08 02:21:07 -0800

    Audio Log E to Close

--- Invasive_1/Assets/Scripts/AudioLogDisplay.cs
+++ Invasive_1/Assets/Scripts/AudioLogDisplay.cs
@@ -2,22 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AudioLogDisplay : MonoBehaviour
 {
     public GameObject audioLog1, audioLog2, audioLog3, audioLog4, audioLog5;
     bool active = false;
     int readCounter = 0;
+    public TextMeshProUGUI pauseText;
     // Start is called before the first frame update
     void Start()
     {
+        pauseText.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
         readCounter--;
-        if(Input.GetKeyDown(KeyCode.E) && active == true && readCounter < 0) modifyDisplay(0);
+        if(Input.GetKeyDown(KeyCode.E) && active == true && readCounter < 0) {
+            modifyDisplay(0);
+            pauseText.text = "";
+        }
     }
     public void modifyDisplay(int i){
 
@@ -35,18 +41,23 @@ public class AudioLogDisplay : MonoBehaviour
             switch(i){
                 case 1:
                     audioLog1.SetActive(true);
+                    pauseText.text = "Press E to Close";
                     break;
                 case 2:
                     audioLog2.SetActive(true);
+                    pauseText.text = "Press E to Close";
                     break;
                 case 3:
                     audioLog3.SetActive(true);
+                    pauseText.text = "Press E to Close";
                     break;
                 case 4:
                     audioLog4.SetActive(true);
+                    pauseText.text = "Press E to Close";
                     break;
                 case 5:
                     audioLog5.SetActive(true);
+                    pauseText.text = "Press E to Close";
                     break;
                 default:
                     break;
